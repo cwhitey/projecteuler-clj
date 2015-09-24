4412f268c1146a6f7577886505c8ba7113da0b2d
Callum White <callum.white@auspost.com.au>
2015-09-24 13:39:09 +1000

    2 works

--- 2.clj
+++ 2.clj
@@ -1,10 +1,11 @@
+(defn problem2 []
+  (loop [one 1
+         two 2
+         sum 0]
+    (if (> two 4000000)
+      sum
+      (if (even? two)
+        (recur two (+ one two) (+ sum two))
+        (recur two (+ one two) sum)))))
 
-
-(loop [one 1
-       two 2
-       sum 0]
-  (if (> two 4000000)
-    sum
-    (if (even? two)
-      (recur two (+ one two) (+ sum two))
-      (recur two (+ one two) sum))))
+(problem2)
