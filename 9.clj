f0b0e4c00001054f2a99ebfdb17b1ac20d9afe14
Callum White <callum.white@auspost.com.au>
2015-09-24 13:40:03 +1000

    9 works

--- 9.clj
+++ 9.clj
@@ -1,18 +1,23 @@
-;A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+;;A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
-;a^2 + b^2 = c^2
-;For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+;;a^2 + b^2 = c^2
+;;For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
-;There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-;Find the product abc.
+;;There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+;;Find the product abc.
 
 (defn square [x] (* x x))
 
-;; Horribly inefficient
-(doseq [a (range 1 1000)
-        b (range 2 1001)
-        c (range 3 1002)]
-  (if (and (= (+ (square a) (square b))
-              (square c))
-           (= 1000 (+ a b c)))
-    (println (* a b c))))
+(defn pythagorean? [a b c]
+  (= (+ (square a) (square b))
+     (square c)))
+
+;; Horribly slow
+(time (apply * (first
+                (for [a   (range 1 1000)
+                      b   (range 2 1001)
+                      c   (range 3 1002)
+                      :when (and (< a b c)
+                                 (= 1000 (+ a b c))
+                                 (pythagorean? a b c))]
+                  [a b c]))))
